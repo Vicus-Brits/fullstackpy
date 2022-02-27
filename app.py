9cbda2ff14bef284dd7654e489857f5551bcadbe
Vicus Brits <vicusbritsmail@gmail.com>
2022-02-27 20:49:10 +0200

    change for website

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, StackOverflow!</p>"
+    return "<p>Hello, StackOverflow!</p><br> <p>Updated via VSCode</p>"
 
 
 # if __name__ == '__main__':
